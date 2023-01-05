24bcb195d2cbbebc860c275a6ab9d350052697e6
MariaPaulaT <mp.tellez@uniandes.edu.co>
2023-01-04 21:43:39 -0500

    Reverse string

--- Arrrays/reverseString.java
+++ Arrrays/reverseString.java
@@ -1,23 +1,40 @@
 package Ejercicios.Arrrays;
-//Create a function that reverses a String 
+import java.util.*; 
+import java.util.Collection;
+//Create a function that reverses a String.
+// Other solution is to create an other array called reverseArray and make a for loop 
+//in which we pass through the letters array and pass it to the reverseArray.
 public class reverseString {
    
-    public static void reverse (String word){
+    public static void reverse1 (String word){
  
+        if (word == null || word.length()<1){
+            System.out.println("This function doesn´t work in that type of parameter");
+        }
         String[] letters = word.split("");
         String[] letter = new String[1];
         letter[0]="a";
-        int leng = letters.length-1;
+        int itemsNumber = letters.length-1;
 
         for (int i =0; i< letters.length/2; i++){
             letter[0]=letters[i];
-            letters[i]= letters[leng]; 
-            letters[leng]= letter[0];
-            leng--;
+            letters[i]= letters[itemsNumber]; 
+            letters[itemsNumber]= letter[0];
+            itemsNumber--;
         }
 
     }
+
+    public static void reverse2(String word){
+        String[] letters =word.split("");
+        Collections.reverse(Arrays.asList(letters));
+        System.out.println(letters[0]);
+        System.out.println(letters[1]);
+        System.out.println(letters[2]);
+        System.out.println(letters[3]);
+
+    }
     public static void main(String[] args) {
-        reverse("heloo");
+        reverse2("heloo");
     }
 }
