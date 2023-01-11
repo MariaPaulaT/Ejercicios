ffe004531daa07a12e6ed26288227022b709953b
MariaPaulaT <mp.tellez@uniandes.edu.co>
2023-01-10 22:13:53 -0500

    Merge arrays

--- Arrrays/MergeSortedArray.java
+++ Arrrays/MergeSortedArray.java
@@ -3,7 +3,14 @@ package Ejercicios.Arrrays;
 // Opcion 1: 
 public class MergeSortedArray {
     
-    public static void MergeArray(int[] numbers1, int[] numbers2){
+    public static int[] MergeArray(int[] numbers1, int[] numbers2){
+        //Check input
+        if (numbers1.length==1){
+            return numbers2;
+        }
+        if (numbers2.length==1){
+            return numbers1;
+        }
         int numb1lenght = numbers1.length;
         int numb2lenght = numbers2.length;
         int leng = numb1lenght +numb2lenght;
@@ -34,16 +41,57 @@ public class MergeSortedArray {
         System.out.println(arreglo[3]);
         System.out.println(arreglo[4]);
         System.out.println(arreglo[5]);
+        return arreglo;
+    }
+
+    public static void mergeArr2 (int[] numb1, int[] numb2){
+
+        int j=0;
+        int i=0;
+        int ar =0;
+        int[] arreglo = new int[numb1.length+ numb2.length];
+        while(i<numb1.length && j< numb2.length){
+            if(numb1[i]>numb2[j]){
+                arreglo[ar]=numb2[j];
+                ar++;
+                j++;
+            }
+            else{
+                arreglo[ar]= numb1[i];
+                ar++;
+                i++;
+            }
+        }
+        if (i<numb1.length){
+            while(i<numb1.length){
+                arreglo[ar]=numb1[i];
+                i++;
+            }
+        }
+
+        if (j<numb2.length){
+            while(j<numb2.length){
+                arreglo[ar]=numb2[j];
+                j++;
+            }
+
+        }
+        System.out.println(arreglo[0]);
+        System.out.println(arreglo[1]);
+        System.out.println(arreglo[2]);
+        System.out.println(arreglo[3]);
+        System.out.println(arreglo[4]);
+        System.out.println(arreglo[5]);
     }
     public static void main(String[] args) {
         int[] nums1= new int[3];
         nums1[0]=5;
-        nums1[1]=8;
+        nums1[1]=6;
         nums1[2]=7;
         int[] nums2 = new int[3];
         nums2[0]=1;
         nums2[1]=3;
         nums2[2]=9;
-        MergeArray(nums1,nums2); 
+        mergeArr2(nums1,nums2); 
     }
 }
